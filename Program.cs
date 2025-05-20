d8d006b429c059fd7fb9fb9be9dbfa1ecb232931
Ramiro <ramiro2k2@gmail.com>
2025-05-20 13:16:07 -0300

    punto1

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+string numeroS;
+
+int resultado;
+
+Console.WriteLine("Ingrese un numero:");
+numeroS=Console.ReadLine();
+
+resultado =InvertirNumero(numeroS); 
+
+Console.WriteLine("Hello, World!"+resultado);
+
+
+
+
+//Funciones
+int InvertirNumero(string numeroS){
+int numeroInvertido = 0;
+int numero;
+if (int.TryParse(numeroS, out numero)){
+
+    while (numero > 0) {
+        int digito = numero % 10;
+        numeroInvertido = numeroInvertido * 10 + digito;
+        numero /= 10;
+    }
+}
+return numeroInvertido;
+}
